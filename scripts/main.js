9bc0aed16cc24dcb61d47ed8649f4715c4b93a94
leprekus <leprekus@yahoo.com>
2022-02-28 08:40:11 -0800

    center image inside contact section

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,16 @@
+//nav vars
+let burgerNavButton = document.getElementById('burger-nav-button')
+let dropDownMenu = document.getElementById('dropdown-menu')
+
+// Section vars
 let contactSection = document.getElementById('contact')
 let projectsSection = document.getElementById('projects')
 let blogSection = document.getElementById('blog')
+
+function toggleVisibility(domElement) {
+
+}
+
+
+burgerNavButton.addEventListener('click', () => toggleVisibility(dropDownMenu))
 
