30981b9d2c25c7bf337bc9f9c18b4e3f84bc3ec8
leprekus <leprekus@yahoo.com>
2022-03-21 21:45:22 -0700

    add function to get page width offset

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,16 @@
 //vars
-let doubleArrow = document.getElementById()
+let doubleArrow = document.getElementById('double-arrow')
+let test = document.querySelectorAll('main .content-wrapper')
+
+function getOffsetTop() {
+    test.forEach(element => {
+        const leftOffset =  element.offsetLeft;
+        const rightOffset = element.offsetRight
+        const elementWidth = element.offsetWidth
+        if (leftOffset + rightOffset < elementWidth) {
+            console.log('small')
+        }
+    })
+}
+
+window.addEventListener('scroll', getOffsetTop)
