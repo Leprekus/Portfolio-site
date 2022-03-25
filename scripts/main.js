bbaeec5bffeae9ccd8bccf7857e1dc9402da224b
Leprekus <leprekus@yahoo.com>
2022-03-24 20:45:33 -0700

    navbubbles work & grid size fits content

--- scripts/main.js
+++ scripts/main.js
@@ -3,8 +3,19 @@ let doubleArrow = document.getElementById('double-arrow')
 let test = document.querySelectorAll('main .content-wrapper')
 
 let sections = document.querySelectorAll('.content-wrapper')
-
+let navBubbles = document.querySelectorAll('.nav-bubbles-container-desktop .nav-bubbles')
 let counter = 0;
+
+function highlightNavBubbles() {
+
+  if(counter === 0) {
+        navBubbles[counter].classList.add('current')
+        navBubbles[navBubbles.length - 1].classList.remove('current')
+    } else {
+        navBubbles[counter].classList.add('current')
+        navBubbles[counter - 1].classList.remove('current')    
+    }    
+}
 
 function changeSection() {
     counter += 1
@@ -12,9 +23,9 @@ function changeSection() {
         counter = 0
     }
     doubleArrow.href = `#${sections[counter].id}`
-  
+    highlightNavBubbles()
+
     // doubleArrow.href = `#${}`
 }
 
 doubleArrow.onclick = changeSection
-
